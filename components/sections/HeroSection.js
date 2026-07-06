4383d0b0dacec564e238f4e07d6d8550b831a24b
Srinivasan R <k2b.srinivas@gmail.com>
2026-07-06 16:42:20 +0530

    Hero section video banner with upload video or youtube & vimeo

--- components/sections/HeroSection.js
+++ components/sections/HeroSection.js
@@ -1,22 +1,99 @@
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
+
+function getEmbedUrl(url) {
+  if (!url) return "";
+
+  // YouTube
+  if (url.includes("youtube.com/watch?v=")) {
+    const id = url.split("v=")[1].split("&")[0];
+    return `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&loop=1&playlist=${id}&controls=0&showinfo=0&rel=0&playsinline=1`;
+  }
+
+  // youtu.be
+  if (url.includes("youtu.be/")) {
+    const id = url.split("youtu.be/")[1];
+    return `https://www.youtube.com/embed/${id}?autoplay=1&mute=1&loop=1&playlist=${id}&controls=0&showinfo=0&rel=0&playsinline=1`;
+  }
+
+  // Vimeo
+  if (url.includes("vimeo.com/")) {
+    const id = url.split("/").pop();
+    return `https://player.vimeo.com/video/${id}?background=1&autoplay=1&loop=1&muted=1`;
+  }
+
+  return url;
+}
+
 export default function HeroSection({ data }) {
   return (
     <section
       className="hero_banner"
       style={{
-        backgroundImage: data?.backgroundImage?.asset?.url
-          ? `url(${data.backgroundImage.asset.url})`
-          : undefined,
+        position: "relative",
+        overflow: "hidden",
+        backgroundImage:
+          data?.backgroundType === "image" &&
+          data?.backgroundImage?.asset?.url
+            ? `url(${data.backgroundImage.asset.url})`
+            : "none",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
+      {/* Uploaded Video */}
+
+      {data?.backgroundType === "video" &&
+        data?.backgroundVideo?.asset?.url && (
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute inset-0 w-full h-full object-cover"
+            style={{
+              zIndex: 0,
+            }}
+          >
+            <source
+              src={data.backgroundVideo.asset.url}
+              type="video/mp4"
+            />
+          </video>
+        )}
+
+      {/* YouTube / Vimeo */}
+
+      {data?.backgroundType === "embed" &&
+        data?.videoUrl && (
+          <iframe
+            src={getEmbedUrl(data.videoUrl)}
+            title="Hero Background Video"
+            allow="autoplay; fullscreen"
+            allowFullScreen
+            frameBorder="0"
+            className="absolute inset-0 w-full h-full"
+            style={{
+              zIndex: 0,
+              pointerEvents: "none",
+            }}
+          />
+        )}
+
       {/* CONTENT */}
 
-      <div className="hero_insides">
+      <div
+        className="hero_insides"
+        style={{
+          position: "relative",
+          zIndex: 2,
+        }}
+      >
         {/* LOGO */}
+
         {data?.logo?.asset?.url && (
           <Image
-            src={data?.logo?.asset?.url}
+            src={data.logo.asset.url}
             alt="Cheap Truck Insurance"
             width={208}
             height={80}
@@ -24,17 +101,31 @@ export default function HeroSection({ data }) {
             className="headlogo"
           />
         )}
+
+        <hr />
+
         {/* TITLE */}
-        <hr></hr>
+
         <h1>{data?.title}</h1>
+
         {/* DESCRIPTION */}
+
         <PortableText value={data?.description?.content} />
+
         {/* BUTTONS */}
+
         <div className="btntwoone">
-          <a href={data?.primaryButtonLink} className="btn_orange">
+          <a
+            href={data?.primaryButtonLink}
+            className="btn_orange"
+          >
             {data?.primaryButtonText}
           </a>
-          <a href={data?.secondaryButtonLink} className="telno">
+
+          <a
+            href={data?.secondaryButtonLink}
+            className="telno"
+          >
             {data?.secondaryButtonText}
           </a>
         </div>
--- sanity/schemaTypes/objects/heroSection.js
+++ sanity/schemaTypes/objects/heroSection.js
@@ -1,58 +1,91 @@
 const heroSection = {
-  name: 'heroSection',
-  title: 'Hero Section',
-  type: 'object',
+  name: "heroSection",
+  title: "Hero Section",
+  type: "object",
 
   fields: [
-
     {
-      name: 'logo',
-      title: 'Logo',
-      type: 'image',
+      name: "logo",
+      title: "Logo",
+      type: "image",
     },
 
     {
-      name: 'backgroundImage',
-      title: 'Background Image',
-      type: 'image',
+      name: "backgroundType",
+      title: "Background Type",
+      type: "string",
+      initialValue: "image",
+      options: {
+        list: [
+          { title: "Image", value: "image" },
+          { title: "Upload Video", value: "video" },
+          { title: "YouTube / Vimeo URL", value: "embed" },
+        ],
+        layout: "radio",
+      },
     },
 
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
+      name: "backgroundImage",
+      title: "Background Image",
+      type: "image",
+      hidden: ({ parent }) => parent?.backgroundType !== "image",
     },
 
     {
-      name: 'description',
-      title: 'Description',
-      type: 'richTextSection',
+      name: "backgroundVideo",
+      title: "Background Video",
+      type: "file",
+      options: {
+        accept: "video/mp4,video/webm",
+      },
+      hidden: ({ parent }) => parent?.backgroundType !== "video",
     },
 
     {
-      name: 'primaryButtonText',
-      title: 'Primary Button Text',
-      type: 'string',
+      name: "videoUrl",
+      title: "YouTube / Vimeo URL",
+      type: "url",
+      hidden: ({ parent }) => parent?.backgroundType !== "embed",
+      description:
+        "Paste a YouTube or Vimeo video URL.",
     },
 
     {
-      name: 'primaryButtonLink',
-      title: 'Primary Button Link',
-      type: 'string',
+      name: "title",
+      title: "Title",
+      type: "string",
     },
 
     {
-      name: 'secondaryButtonText',
-      title: 'Secondary Button Text',
-      type: 'string',
+      name: "description",
+      title: "Description",
+      type: "richTextSection",
     },
 
     {
-      name: 'secondaryButtonLink',
-      title: 'Secondary Button Link',
-      type: 'string',
+      name: "primaryButtonText",
+      title: "Primary Button Text",
+      type: "string",
     },
 
+    {
+      name: "primaryButtonLink",
+      title: "Primary Button Link",
+      type: "string",
+    },
+
+    {
+      name: "secondaryButtonText",
+      title: "Secondary Button Text",
+      type: "string",
+    },
+
+    {
+      name: "secondaryButtonLink",
+      title: "Secondary Button Link",
+      type: "string",
+    },
   ],
 };
 
