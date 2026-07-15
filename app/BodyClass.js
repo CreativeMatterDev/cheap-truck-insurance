6cfc4e43e94f619d4a9b76aecf8f49bf0d0c3650
Srinivasan R <k2b.srinivas@gmail.com>
2026-07-15 14:04:40 +0530

    Sticky Header issue debugging with direct URL home class

--- app/BodyClass.js
+++ app/BodyClass.js
@@ -5,13 +5,15 @@ import { usePathname } from "next/navigation";
 export default function BodyClass({ children }) {
   const pathname = usePathname();
 
+  console.log("pathname:", pathname);
+
+  const isHome = pathname === "/" || pathname === "";
+
   return (
     <body
-      className={`min-h-full flex flex-col 
-        ${pathname === "https://cheaptruckinsurance.com.au" ? "home" : ""}
-        ${pathname === "/home" ? "home" : ""}
-        ${pathname === "/contact" ? "contact" : ""}
-      `}
+      className={`min-h-full flex flex-col ${
+        isHome ? "home" : ""
+      } ${pathname === "/contact" ? "contact" : ""}`}
     >
       {children}
     </body>
